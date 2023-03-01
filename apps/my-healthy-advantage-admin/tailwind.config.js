7b9d54d0c6bd83f20a0e041aa9a7aa401e2363d0
Antony Scott <antony.scott@codurance.com>
2023-03-01 08:45:18 +0000

    add tailwindcss to admin project add i18next to admin project fix package.json/scripts in admin project

---
+++ apps/my-healthy-advantage-admin/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+const config = require("@brighthr/tailwind-config");
+
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "../../node_modules/@brighthr/*/src/*",
+    ],
+    theme: {
+        extend: {
+            ...config,
+        },
+        plugins: [],
+    },
+};
